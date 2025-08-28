1ba6648a91a30df052eaaffdbffe085808f5c8f9
Hanoodoo7 <mohalmaskati@gmail.com>
2025-08-28 13:47:38 +0300

    added a post model and starting on the controller

---
+++ models/post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const postSchema = new mongoose.Schema({
+
+    post_id:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    user_id:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    content:{
+        type: String,
+        required: true,
+    },
+    likes:{
+        type: Number
+    }
+},
+  {
+    timestamps: true,
+  }
+);
